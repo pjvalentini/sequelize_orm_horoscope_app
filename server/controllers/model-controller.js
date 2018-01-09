9b57e833b7c505b172e808f42dc5ff022a5026f5
pjvalentini <pj.valentini@gmail.com>
2018-01-08 21:56:50 -0500

    get-users route and create-users route is working

--- server/controllers/model-controller.js
+++ server/controllers/model-controller.js
@@ -4,42 +4,42 @@ var models = require('./../models');
 module.exports = {
 	getAllUsers: (users) => {
 		models.User.findAll({
-			attributes: ['name', 'birthdate'],
+			attributes: ['name', 'birthdate', 'zodiac'],
 		}).then(function(res) {
 			users(res);
 		});
 	},
-};
 
-createUser: (name, birthdate, zodiac, newUser) => {
-	models.User.create({
-		name: name,
-		birthdate: birthdate,
-		zodiac: zodiac,
-	}).then((res) => {
-		newUser(res);
-	});
-};
+	createUser: (name, birthdate, zodiac, newUser) => {
+		models.User.create({
+			name: name,
+			birthdate: birthdate,
+			zodiac: zodiac,
+		}).then((res) => {
+			newUser(res);
+		});
+	},
 
-getAllZodiacs: (signs) => {
-	models.Horoscope.findAll({}).then((res) => {
-		signs(res);
-	});
-};
+	getAllZodiacs: (signs) => {
+		models.Horoscope.findAll({}).then((res) => {
+			signs(res);
+		});
+	},
 
-getOneZodiac: (id , signs) => {
-	models.Horoscope.findAll({ where: { id: id } }).then((res) => {
-		signs(res);
-	});
-};
+	getOneZodiac: (id , signs) => {
+		models.Horoscope.findAll({ where: { id: id } }).then((res) => {
+			signs(res);
+		});
+	},
 
-createZodiac: (zodiac, todays_horoscope, description, date_range, zodiacData) => {
-	models.Horoscope.create({
-		zodiac: zodiac,
-		todays_horoscope: todays_horoscope,
-		description: description,
-		date_range: date_range,
-	}).then((res) => {
-		zodiacData(res);
-	});
+	createZodiac: (zodiac, todays_horoscope, description, date_range, zodiacData) => {
+		models.Horoscope.create({
+			zodiac: zodiac,
+			todays_horoscope: todays_horoscope,
+			description: description,
+			date_range: date_range,
+		}).then((res) => {
+			zodiacData(res);
+		});
+	},
 };
--- server/controllers/route-controller.js
+++ server/controllers/route-controller.js
@@ -3,12 +3,32 @@ var express = require('express');
 var router = express.Router();
 var mc = require('./model-controller.js');
 
-router.get('get-users', function(req, res) {
+var models = require('./../models');
+
+router.get('/get-users', function(req, res) {
 	mc.getAllUsers((users) => {
 		// console.log(users);
 		res.json(users);
 	});
 });
 
-// ALWAYS FUCKING MAKE SURE THIS IS THERE!!!
+router.post('/create-user', (req, res) => {
+	mc.createUser(
+		req.body.name,
+		req.body.birthdate,
+		req.body.zodiac,
+		(user) => {
+			console.log(user);
+			res.json(user);
+		}
+	);
+});
+
+router.get('/zodiac', function(req, res) {
+	mc.getAllZodiacs((signs) => {
+		// console.log(signs);
+		res.json(signs);
+	});
+});
+
 module.exports = router;
